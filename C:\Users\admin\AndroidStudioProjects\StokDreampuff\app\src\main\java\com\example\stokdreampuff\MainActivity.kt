b7a939f8330cad9a5607b99f693321d5e4e3a7c1
Aditya Rifky <em2vine123@gmail.com>
2025-07-03 06:03:30 +0000

    e: file:///C:/Users/admin/AndroidStudioProjects/StokDreampuff/app/src/ma

--- C:\Users\admin\AndroidStudioProjects\StokDreampuff\app\src\main\java\com\example\stokdreampuff\MainActivity.kt
+++ C:\Users\admin\AndroidStudioProjects\StokDreampuff\app\src\main\java\com\example\stokdreampuff\MainActivity.kt
@@ -1,10 +1,10 @@
-
 package com.example.stokdreampuff
 
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.example.stokdreampuff.R // <-- Ini baris import baru yang krusial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         // Baris ini mengaktifkan JavaScript, wajib agar React bisa berjalan
         myWebView.settings.javaScriptEnabled = true
         
-        // Memuat URL aplikasi web dari URL dasar
-        myWebView.loadUrl("https://stokharianproduk.vercel.app/")
+        // Menggunakan URL yang benar, dimulai dari halaman utama (login)
+        myWebView.loadUrl("https://stokharianproduk.vercel.app/") 
     }
 }
